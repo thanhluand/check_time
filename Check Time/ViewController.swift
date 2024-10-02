7124a62436a02c0920ca29b16f7a695e76f4ec18
LuanDT <thanhluand@gmail.com>
2024-10-02 20:04:56 +0900

    first commit

---
+++ Check Time/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Check Time
+//
+//  Created by Dang Luan on 2024/09/19.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
